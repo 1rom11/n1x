2447e1cb47323a7fe667274a9fe4c6687bbca4f8
1rom11dev <76579908+1rom11@users.noreply.github.com>
2021-12-01 15:35:01 +0000

    Updating help.js

--- main.bash
+++ main.bash
@@ -10,7 +10,7 @@ echo "
 2. Open replit.nix 
 Do H for Help " h12e
 		case $h12e in
-			[Hh]* ) node index.html;;
+			[Hh]* ) node help/index.js;;
 			[1]* ) bash app.bash;;
 			[2]* ) nano replit.nix;;
 			[Ee]* ) echo "Exited | Do bash main.bash to open again" && exit;;
